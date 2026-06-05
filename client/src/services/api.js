04983696200c4f1e7abcd59b0c5498ce3c8e43df
Siddharth Jaiswal <siddharthjaiswal7071@gmail.com>
2026-06-05 10:31:05 +0530

    Updated production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/v1"
+  baseURL: "https://personal-finance-tracker-1-1rp5.onrender.com/api/v1"
 });
 
 // Attach token automatically
