e511dadf043258c09e3cee54d9df9559327cee23
Siddharth Jaiswal <siddharthjaiswal7071@gmail.com>
2026-06-05 10:48:34 +0530

    Updated production CORS

--- server/src/app.js
+++ server/src/app.js
@@ -20,7 +20,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://personal-finance-tracker-9qvi.vercel.app/login"
+    ],
     credentials: true,
   })
 );
